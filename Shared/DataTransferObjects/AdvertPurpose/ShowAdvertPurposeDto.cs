caefe801c7ca69c92563781d0ad59c9afb2b77ca
Krsto Kostic <kostic.krsto@gmail.com>
2023-02-10 20:48:17 +0100

    adding translate name in query

--- Shared/DataTransferObjects/AdvertPurpose/ShowAdvertPurposeDto.cs
+++ Shared/DataTransferObjects/AdvertPurpose/ShowAdvertPurposeDto.cs
@@ -3,5 +3,6 @@ public class ShowAdvertPurposeDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Name_En { get; set; }
 }
 
--- Shared/DataTransferObjects/AdvertType/ShowAdvertTypeDto.cs
+++ Shared/DataTransferObjects/AdvertType/ShowAdvertTypeDto.cs
@@ -3,4 +3,6 @@ public class ShowAdvertTypeDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Name_En { get; set; }
+
 }
