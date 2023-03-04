08c97c84064930e081386a4c06c2a050ad379886
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-04 17:40:42 +0100

    Making handler

---
+++ Application/Handlers/UserHandlers/GetUserHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.UserQueries;
+using Contracts;
+using MediatR;
+
+namespace Application.Handlers.UserHandlers;
+internal sealed class GetUserHandler : IRequestHandler<GetUserQuery, Unit>
+{
+    private readonly IRepositoryManager _repository;
+
+    public GetUserHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(GetUserQuery request, CancellationToken cancellationToken)
+    {
+        await _repository.User.BanUser(request.UserId, request.NoOfDays);
+
+        return Unit.Value;
+    }
+}
