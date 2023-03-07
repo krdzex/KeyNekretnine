922ed1e80c738a0b5824e1489deb43c1ab27af04
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-07 01:08:45 +0100

    Make handler

---
+++ Application/Handlers/AdvertHandlers/GetMyAdvertsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Queries.AdvertQueries;
+using Contracts;
+using MediatR;
+using Shared.CustomResponses;
+using Shared.DataTransferObjects.Advert;
+
+namespace Application.Handlers.AdvertHandlers;
+internal sealed class GetMyAdvertsHandler : IRequestHandler<GetMyAdvertsQuery, Pagination<MinimalInformationsAboutAdvertDto>>
+{
+    private readonly IRepositoryManager _repository;
+
+    public GetMyAdvertsHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Pagination<MinimalInformationsAboutAdvertDto>> Handle(GetMyAdvertsQuery request, CancellationToken cancellationToken)
+    {
+        var userId = _repository.User.GetUserIdFromEmail(request.Email);
+
+        var adverts = await _repository.Advert.GetAdverts(request.AdvertParameters, cancellationToken);
+
+        return adverts;
+    }
+}
