f3335718ab0e5a3cfbc6963be25b061bfb1b9f86
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-09 18:48:27 +0100

    Change url

--- Service/EmailService.cs
+++ Service/EmailService.cs
@@ -21,7 +21,7 @@ internal sealed class EmailService : IEmailService
         var fromName = sendGridConfigSection.GetSection("FromName").Value;
         var templateId = sendGridConfigSection.GetSection("EmailTemplateId").Value;
 
-        var confirmationlink = "https://localhost:7000/api/user/ConfirmEmail?token=" + token + "&email=" + email;
+        var confirmationlink = "https://agencija108.fly.dev/api/user/ConfirmEmail?token=" + token + "&email=" + email;
 
         var client = new SendGridClient(Environment.GetEnvironmentVariable("SEND_GRID_API_KEY"));
 
