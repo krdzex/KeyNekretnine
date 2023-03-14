b1a20686f68a10242d4e2cb6f6c2c6e841339b39
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-14 20:22:42 +0100

    Make handler

---
+++ Application/Handlers/AdvertHandlers/ReportAdvertHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Commands.AdvertCommands;
+using Contracts;
+using MediatR;
+
+namespace Application.Handlers.AdvertHandlers;
+internal sealed class ReportAdvertHandler : IRequestHandler<ReportAdvertCommand, Unit>
+{
+    private readonly IRepositoryManager _repository;
+
+    public ReportAdvertHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(ReportAdvertCommand request, CancellationToken cancellationToken)
+    {
+        //if (request.AdvertParameters.MaxPrice < request.AdvertParameters.MinPrice)
+        //{
+        //    throw new BadPriceException();
+        //}
+
+        //if (request.AdvertParameters.MaxFloorSpace < request.AdvertParameters.MinFloorSpace)
+        //{
+        //    throw new BadFloorSpaceException();
+        //}
+
+        //var adverts = await _repository.Advert.GetAdverts(request.AdvertParameters, cancellationToken);
+
+        return Unit.Value;
+    }
+}
+
