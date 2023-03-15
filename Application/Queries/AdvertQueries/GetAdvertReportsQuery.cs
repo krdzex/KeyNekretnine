1f8a4633513dbde5b0853523aca4bf123d6364dd
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-15 16:40:57 +0100

    Make handler and query

---
+++ Application/Queries/AdvertQueries/GetAdvertReportsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using Shared.DataTransferObjects.Advert;
+
+namespace Application.Queries.AdvertQueries;
+public sealed record GetAdvertReportsQuery() : IRequest<IEnumerable<AdvertReportsDto>>;
