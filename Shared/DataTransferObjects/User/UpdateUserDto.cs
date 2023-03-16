6312c40aa06f922aeff69c13d3378a2b535b20d4
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-16 22:24:02 +0100

    Dto for update user

---
+++ Shared/DataTransferObjects/User/UpdateUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.DataTransferObjects.User;
+public class UpdateUserDto
+{
+    public string About { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
