cc2db982883f064f456c4a5f4dba6f1c9d1f3253
Krsto Kostic <kostic.krsto@gmail.com>
2023-03-22 23:46:26 +0100

    make dto

---
+++ Shared/DataTransferObjects/Advert/UpdateAdvertLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DataTransferObjects.Advert;
+public class UpdateAdvertLocation
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Street { get; set; }
+    public int NeighborhoodId { get; set; }
+
+}
