88a2b8962cbbcf6bdfaf77a1098738de0b3b3d40
Krsto Kostic <kostic.krsto@gmail.com>
2023-06-26 14:18:45 +0200

    Deploy everything now

--- Application/Core/Agencies/Commands/UpdateAgency/UpdateAgencyHandler.cs
+++ Application/Core/Agencies/Commands/UpdateAgency/UpdateAgencyHandler.cs
@@ -4,6 +4,7 @@ using Entities.DomainErrors;
 using MediatR;
 using Service.Contracts;
 using Shared.Error;
+using System.Net;
 using System.Transactions;
 
 namespace Application.Core.Agencies.Commands.UpdateAgency;
@@ -43,39 +44,39 @@ internal sealed class UpdateAgencyHandler : ICommandHandler<UpdateAgencyCommand,
                 return Result.Failure<Unit>(DomainErrors.Agency.NotOwnerError);
             }
 
-            //request.UpdateAgency.ImageUrl = await _repository.Agency.GetAgencyImage(request.AgencyId, cancellationToken);
+            request.UpdateAgency.ImageUrl = await _repository.Agency.GetAgencyImage(request.AgencyId, cancellationToken);
 
-            //if (request.UpdateAgency.Image?.Length > 0)
-            //{
-            //    var tempProfileImageUrl = request.UpdateAgency.ImageUrl;
+            if (request.UpdateAgency.Image?.Length > 0)
+            {
+                var tempProfileImageUrl = request.UpdateAgency.ImageUrl;
 
-            //    request.UpdateAgency.ImageUrl = await _service.ImageService.UploadImageOnCloudinary(request.UpdateAgency.Image);
+                request.UpdateAgency.ImageUrl = await _service.ImageService.UploadImageOnCloudinary(request.UpdateAgency.Image);
 
-            //    if (tempProfileImageUrl is not null)
-            //    {
-            //        var publicId = _service.ImageService.GetPublicIdFromUrl(tempProfileImageUrl);
+                if (tempProfileImageUrl is not null)
+                {
+                    var publicId = _service.ImageService.GetPublicIdFromUrl(tempProfileImageUrl);
 
-            //        var deleteResult = await _service.ImageService.DeleteImageFromCloudinary(publicId);
+                    var deleteResult = await _service.ImageService.DeleteImageFromCloudinary(publicId);
 
-            //        if (deleteResult.StatusCode != HttpStatusCode.OK)
-            //        {
-            //            return Result.Failure<Unit>(DomainErrors.ImageService.ImageCouldntBeDeleted);
-            //        }
-            //    }
-            //}
+                    if (deleteResult.StatusCode != HttpStatusCode.OK)
+                    {
+                        return Result.Failure<Unit>(DomainErrors.ImageService.ImageCouldntBeDeleted);
+                    }
+                }
+            }
 
-            //await _repository.Agency.UpdateAgency(request.UpdateAgency, request.AgencyId, cancellationToken);
+            await _repository.Agency.UpdateAgency(request.UpdateAgency, request.AgencyId, cancellationToken);
 
-            //await _repository.Agency.DeleteLanguagesForAgency(request.AgencyId, cancellationToken);
+            await _repository.Agency.DeleteLanguagesForAgency(request.AgencyId, cancellationToken);
 
-            //if (request.UpdateAgency.LanguageId is not null)
-            //{
-            //    foreach (var languageId in request.UpdateAgency.LanguageId)
-            //    {
-            //        await _repository.Agency.AddLanguageToAgency(languageId, request.AgencyId, cancellationToken);
+            if (request.UpdateAgency.LanguageId is not null)
+            {
+                foreach (var languageId in request.UpdateAgency.LanguageId)
+                {
+                    await _repository.Agency.AddLanguageToAgency(languageId, request.AgencyId, cancellationToken);
 
-            //    }
-            //}
+                }
+            }
 
             transaction.Complete();
         }
