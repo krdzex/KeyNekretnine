3a31f334f889c399578b0dbdbd5faa3141bfa88a
Krsto Kostic <kostic.krsto@gmail.com>
2023-06-26 17:24:29 +0200

    Adding changes to agency query

--- Shared/DataTransferObjects/Agency/GetAgenciesDto.cs
+++ Shared/DataTransferObjects/Agency/GetAgenciesDto.cs
@@ -1,7 +1,16 @@
 ﻿namespace Shared.DataTransferObjects.Agency;
 public class GetAgenciesDto
 {
+
     public string Name { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Twitter_Url { get; set; }
+    public string Facebook_Url { get; set; }
+    public string Instagram_Url { get; set; }
+    public string Linkedin_Url { get; set; }
+    public string Image_Url { get; set; }
+
     public DateTime Created_Date { get; set; }
     public int Num_Adverts { get; set; }
 }
